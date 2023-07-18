273c1e51a389cf2bad2d05bfaeb485f292067c63
Ifanfomin <129762316+Ifanfomin@users.noreply.github.com>
2023-07-18 10:17:35 +0300

    Импортировал vk_token и счётчик участников

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from save_group_users import SaveGroupUsers
 from clear_data_base import ClearDataBase
 from show_data_base_tables import ShowDataBaseTables
-from config import token, host, user, password, database
+from number_ofUsers_in_group import NumberOfUsersInGroup
+from config import vk_token, host, user, password, database
 
 if __name__ == "__main__":
-    SaveGroupUsers("id_группы", token, host, user, password, database)
+    SaveGroupUsers("id_группы", vk_token, host, user, password, database)
     ShowDataBaseTables(host, user, password, database)
