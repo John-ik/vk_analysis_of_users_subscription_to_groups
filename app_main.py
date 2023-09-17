b4f63343913d360b5d661ecc8b2c00d1f4664cc2
Ifanfomin <129762316+Ifanfomin@users.noreply.github.com>
2023-09-17 19:21:55 +0300

    Программа на flet с панелью слева
    Навигационные бар слева и более удобный выбор с просмотром пересечений

--- app_main.py
+++ app_main.py
@@ -2,6 +2,7 @@ import flet as ft
 from config import host, user, password, database, vk_token
 from classes_main import *
 
+
 def main(page: ft.Page):
     def groups_ft_table():
         sql_column = ShowDataBaseTables(host, user, password, database).groups
@@ -10,28 +11,41 @@ def main(page: ft.Page):
             data_row = ft.DataRow(
                 cells=[
                     ft.DataCell(ft.Text(line[0])),
-                    ft.DataCell(ft.Text(line[1]))
+                    ft.DataCell(ft.Text(
+                        color=ft.colors.BLUE,
+                        spans=[ft.TextSpan(
+                                text=line[1][0:-1],
+                                url=f"https://vk.com/{line[1]}"
+                                    )]))
                 ]
             )
             lines.append(data_row)
 
-        ft_column = ft.DataTable(
+        ft_table = ft.DataTable(
+            width=1700,
             columns=[
                 ft.DataColumn(ft.Text("Имя группы")),
                 ft.DataColumn(ft.Text("ID группы"))
             ],
             rows=lines)
-        return ft_column
+        column =ft.Column(controls=[ft_table], scroll=ft.ScrollMode.ALWAYS, height=900)
+        return column
 
     def search_intersections_screen():
-        def make_ft_data_table(users):
-            if len(column_list) > 2 + groups_count + 1:
-                column_list.pop()
+        def make_ft_data_table(users, searching_groups_count):
+            intersections_count = 0
             lines = []
             for user in users:
+                intersections_count += 1
                 data_row = ft.DataRow(
                     cells=[
-                        ft.DataCell(ft.Text(user[0])),
+                        ft.DataCell(
+                            ft.Text(color=ft.colors.BLUE,
+                                    spans=[ft.TextSpan(
+                                        text=user[0],
+                                        url=f"https://vk.com/id{int(user[0])}"
+                                    )]),
+                        ),
                         ft.DataCell(ft.Text(user[1])),
                         ft.DataCell(ft.Text(user[2])),
                         ft.DataCell(ft.Text(user[3]))
@@ -45,8 +59,18 @@ def main(page: ft.Page):
                     ft.DataColumn(ft.Text("Дата рождения")),
                     ft.DataColumn(ft.Text("Место обучения"))
                 ],
-                rows=lines)
-            column_list.append(ft_table)
+                rows=lines
+            )
+
+            list_information = [ft.Text(f"Количество групп: {groups_count}", size=20),
+                                 ft.Text(f"Количество выбранных групп: {searching_groups_count}", size=20),
+                                 ft.Text(f"Koличество пересечений: {intersections_count}", size=20),
+                                 ft_table
+                                 ]
+            information_column = ft.Column(spacing=10, controls=list_information,
+                                                 scroll=ft.ScrollMode.ALWAYS,
+                                                 height=900)
+            page_row.controls[1] = information_column
 
 
         def start_search_intersections(e):
@@ -54,115 +78,118 @@ def main(page: ft.Page):
             searching_groups = []
             for line_i in range(len(column_list)):
                 group_id = ""
-                if 1 < line_i < groups_count + 2:
+                if 0 < line_i < groups_count + 1:
                     if column_list[line_i].value == True:
-                        for char in column_list[line_i].label[-2::-1]:
-                            if char != '"':
-                                group_id += char
-                            else:
-                                break
-                        group_id = group_id[::-1]
+                        group_id = column_list[line_i].label.split('"')[3]
                 if group_id != "":
                     searching_groups.append(group_id)
+
             print(searching_groups)
+            searching_groups_count = len(searching_groups)
+
             if searching_groups:
                 users = SearchForIntersections(searching_groups, host, user, password, database).users
                 print(users)
-                make_ft_data_table(users)
-                check_box_column.update()
+                make_ft_data_table(users, searching_groups_count)
+                page_row.update()
 
         print("экран выбора групп для поиска пересечений")
         sql_column = ShowDataBaseTables(host, user, password, database).groups
-        column_list = [ft.Text(""), ft.Text("Выбор группы для поиска пересечений")]
+        column_list = [ft.Text("Выбор группы для поиска пересечений")]
         groups_count = 0
         for line in sql_column:
             column_list.append(ft.Checkbox(label=f'"{line[0]}" "{line[1]}"', value=False))
             groups_count += 1
+
+        users_table = ft.DataTable(
+                columns=[
+                    ft.DataColumn(ft.Text("ID")),
+                    ft.DataColumn(ft.Text("Имя Фамилия")),
+                    ft.DataColumn(ft.Text("Дата рождения")),
+                    ft.DataColumn(ft.Text("Место обучения"))
+                ]
+        )
+        searching_group_count = 0
+        intersections_count = 0
+        list_information = [ft.Text(f"Количество групп: {groups_count}", size=20),
+                             ft.Text(f"Количество выбранных групп: {searching_group_count}", size=20),
+                             ft.Text(f"Koличество пересечений: {intersections_count}", size=20),
+                             users_table
+                             ]
+        information_column = ft.Column(spacing=10, controls=list_information, scroll=ft.ScrollMode.ALWAYS,
+                                             height=900)
+
         submit_button = ft.ElevatedButton(text="Поиск", on_click=start_search_intersections)
         column_list.append(submit_button)
-        check_box_column = ft.Column(spacing=10, controls=column_list, height=400)
-        return check_box_column
+        check_box_column = ft.Column(spacing=10, controls=column_list, scroll=ft.ScrollMode.ALWAYS, height=900)
+        page_row = ft.Row([check_box_column, information_column])
+        return page_row
 
-    def ft_delete_group():
-        def delete_from_screen(group_id, groups_count):
-            for check_box_id in range(2, len(column_list) - 2):
-                check_box_group_id = column_list[check_box_id].label.split('"')[3]
-                if check_box_group_id == group_id:
-                    print(group_id)
-                    column_list.pop(check_box_id)
-                    print(len(column_list))
-                    check_box_column.update()
-                    groups_count -= 1
-                    return groups_count
+    pages = [
+        groups_ft_table(),
+        search_intersections_screen(),
+        ft.Text(visible=False,
+                color=ft.colors.BLUE,
+                spans=[ft.TextSpan(
+                    "GitHub проекта!",
+                    url="https://github.com/Ifanfomin/vk_analysis_of_users_subscription_to_groups"
+                )]),
+    ]
 
-        def delete_groups(e):
-            print("Поиск начат")
-            groups_to_delete = []
-            for line_i in range(len(column_list)):
-                group_id = ""
-                if 1 < line_i < groups_count + 2:
-                    if column_list[line_i].value == True:
-                        print(len(column_list))
-                        for char in column_list[line_i].label[-2::-1]:
-                            if char != '"':
-                                group_id += char
-                            else:
-                                break
-                        group_id = group_id[::-1]
-                if group_id != "":
-                    groups_to_delete.append(group_id)
-            print(groups_to_delete)
-            if groups_to_delete:
-                for group_id in groups_to_delete:
-                    # DeleteGroupFromBase(group_id, host, user, password, database)
-                    groups_count = delete_from_screen(group_id, groups_count)
-                print(groups_to_delete, "удален")
+    def select_page():
+        print(f"Страница {rail.selected_index}")
+        for index, p in enumerate(pages):
+            p.visible = True if index == rail.selected_index else False
+        page.update()
 
-        print("экран выбора групп для удаления")
-        sql_column = ShowDataBaseTables(host, user, password, database).groups
-        column_list = [ft.Text(""), ft.Text("Выбор группы для удаления")]
-        groups_count = 0
-        for line in sql_column:
-            column_list.append(ft.Checkbox(label=f'"{line[0]}" "{line[1]}"', value=False))
-            groups_count += 1
-        submit_button = ft.ElevatedButton(text="Удалить", on_click=delete_groups)
-        column_list.append(submit_button)
-        check_box_column = ft.Column(spacing=10, controls=column_list, height=400)
-        return check_box_column
+    def dest_change(e):
+        select_page()
 
     page.title = "поиск участников групп вконтакте"
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.scroll = "auto"
-    page.window_height = 600
-    page.window_width = 800
-    # save_group_button = ft.ElevatedButton("Добавить группу в базу", on_click=search_groups_screen)
-    # show_groups_button = ft.ElevatedButton("Список групп в базе", on_click=show_groups_screen)
-    # delete_group_button = ft.ElevatedButton("Удалить группу из базы", on_click=delete_group_screen)
-    # search_intersections_button = ft.ElevatedButton("Поиск пересечений групп", on_click=search_intersections_screen)
-    # line_1 = ft.Row(controls=[save_group_button, show_groups_button, delete_group_button, search_intersections_button])
-    tabs = ft.Tabs(
-        selected_index=0,
-        animation_duration=300,
-        tabs=[
-            ft.Tab(
-                text="Список групп в базе",
-                content=groups_ft_table()
+
+
+    column = groups_ft_table()  # ft.Column([ft.Text("Body!")], auto_scroll=True)  # alignment=ft.MainAxisAlignment.START, expand=True
+
+    rail = ft.NavigationRail(
+        selected_index=1,
+        label_type=ft.NavigationRailLabelType.ALL,
+        # extended=True,
+        min_width=100,
+        min_extended_width=0,
+        group_alignment=-0.2,
+        destinations=[
+            ft.NavigationRailDestination(
+                icon=ft.icons.TABLE_ROWS, label="Список групп"
             ),
-            ft.Tab(
-                text="Поиск пересечений групп",
-                content=search_intersections_screen()
+            ft.NavigationRailDestination(
+                icon=ft.icons.MANAGE_SEARCH,
+                label="Пересечения",
             ),
-            ft.Tab(
-                text="Добавить группу в базу",
-                content=groups_ft_table()
+            ft.NavigationRailDestination(
+                icon=ft.icons.PLAYLIST_ADD,
+                label_content=ft.Text("Добавить"),
             ),
-            ft.Tab(
-                text="Удалить группу из базы",
-                content=ft_delete_group()
-            )
-        ]
+        ],
+        on_change=dest_change,
     )
-    page.add(tabs)
-    page.update()
+
+    select_page()
+
+    page.window_height = 1080
+    page.window_width = 1920
+
+    page.add(
+        ft.Row(
+            [
+                rail,
+                ft.VerticalDivider(width=1),
+                ft.Row(pages, expand=True, scroll=ft.ScrollMode.ALWAYS)
+            ],
+            expand=True
+        )
+    )
+
+    # update_page(column)
+
 
 ft.app(target=main)
