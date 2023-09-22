0120c91ad85e1d9dd3f7bd2a3f43b048772edab7
Johnik <94196588+John-ik@users.noreply.github.com>
2023-09-22 18:43:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from classes_main import *
 from config import vk_token, host, user, password, database
 
 if __name__ == "__main__":
-    SaveGroupUsers("club34956621", vk_token, host, user, password, database)
+    SaveGroupUsers("id_group", vk_token, host, user, password, database)
     ShowDataBaseTables(host, user, password, database)
